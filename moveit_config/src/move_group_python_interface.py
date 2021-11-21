b70186914c0ef17eae8f296e38dd94fbd91cd6e0
Lakshan-Vibhusha-Kannangara <77208138+Lakshan-Vibhusha-Kannangara@users.noreply.github.com>
2021-11-21 13:46:52 +0530

    Add files via upload

---
+++ moveit_config/src/move_group_python_interface.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()    
+group = moveit_commander.MoveGroupCommander("arm")
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory)
+
+group_variable_values = geometry_msgs.msg.Pose()
+
+
+
+group.set_named_target("pose_2")
+
+plan1 = group.plan()
+
+
+rospy.sleep(5)
+
+moveit_commander.roscpp_shutdown()
